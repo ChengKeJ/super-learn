7596601c30788fe5b496abfbaf05ae3afc17bf71
ChengKeJ <ischeng.kejun@gmail.com>
2020-09-14 20:12:54 +0800

    future task simple use

---
+++ super-learn-base/src/main/java/com/ckj/base/thread/FutureTaskCore.java
@@ -0,0 +1,55 @@
+package com.ckj.base.thread;
+
+import java.util.concurrent.*;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author c.kj
+ * @Description Future simple use
+ * @Date 2020-09-14
+ * @Time 19:46
+ * @Copyright @2019 Zhongan.com All right reserved
+ **/
+@Slf4j
+public class FutureTaskCore {
+
+    private static final FutureTaskCore INSTANCE = new FutureTaskCore();
+
+    public void futureTask() throws ExecutionException, InterruptedException {
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        // no future returned
+        FutureTask<String> future = new FutureTask<>(() -> {
+            log.info("start task ....");
+            Thread.sleep(1000);
+            log.info("end task ...");
+            return "ok";
+        });
+        executor.execute(future);
+
+        // have returned future
+        Future<String> submit = executor.submit(() -> {
+            log.info("start task2 ....");
+            Thread.sleep(1000);
+            log.info("end task2...");
+            return "ok";
+        });
+        log.info("start another task ...");
+
+        if ("ok".equals(submit.get())) {
+            log.info("over start another task2 ...");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            FutureTaskCore.INSTANCE.futureTask();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
